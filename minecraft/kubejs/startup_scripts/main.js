a9b31631f2f7d3089e33f5cf6069bdb3245788dc
pink <robin@voit-consulting.de>
2026-05-06 22:33:44 +0200

    removing left over comments

--- minecraft/kubejs/startup_scripts/main.js
+++ minecraft/kubejs/startup_scripts/main.js
@@ -1,11 +1,7 @@
-// Visit the wiki for more info - https://kubejs.com/
-//custom materials ----------------------------------------
 
-// Listen to item registry event
+//custom materials ----------------------------------------
 StartupEvents.registry('item', event => {
-  // The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png
-  // If you want a custom item model, you can create one in Blockbench and put it in kubejs/assets/kubejs/models/item/test_item.json
- 
+  
 event.create('arcane_gem').texture('gems:item/material_crystal_arcane_gem')
 event.create('copperbound_prism').texture('gems:item/gem_prism_copperbound')
 event.create('blood_essence').texture('gems:item/magic_essence_blood_gem_medium')
@@ -15,12 +11,4 @@ event.create('rought_sapphire').texture('gems:item/gem_sapphire_rough')
 event.create('sapphire_nugget').texture('gems:item/gem_sapphire_nugget')
 event.create('medium_sapphire').texture('gems:item/gem_sapphire_medium')
 
-  // If you want to specify a different texture location you can do that too, like this:
-  //event.create('test_item_1').texture('mobbo:item/lava') // This texture would be located at kubejs/assets/mobbo/textures/item/lava.png
-
-  // You can chain builder methods as much as you like
-  //event.create('test_item_2').maxStackSize(16).glow(true)
-
-  // You can specify item type as 2nd argument in create(), some types have different available methods
-  //event.create('custom_sword', 'sword').tier('diamond').attackDamageBaseline(10)
 })
